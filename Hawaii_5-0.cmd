8c7255f1e85a29b0aa6ece591ac15a3a8b1277ee
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-01 10:50:42 -1000

    Add files via upload

---
+++ Hawaii_5-0.cmd
@@ -0,0 +1,33 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "Hawaii_5-0"
+@cecho off
+@set "apk=192681346781"
+@set "app=194321789678"
+@set "iso=192341678321"
+cecho {09}1=apk{/h}2=app{/h}3=iso{04}
+
+set /a m=Choose;
+if (%m%=1) & goto one
+if (%m%=2) & goto two
+if (%m%=3) & goto three
+goto top
+
+:one
+copy "%apk% ./police.apk" 
+goto 7L
+
+:two
+copy "%app% ./police.app"
+goto 7L
+
+:three
+copy "%iso% ./police.iso"
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+@timeout /t 5
+goto top
