3484e6b26a1a42c8fd47716a03de359ae8d7a5ce
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-25 12:45:13 -1000

    Update Hawaii_5-0.cmd

--- Hawaii_5-0.cmd
+++ Hawaii_5-0.cmd
@@ -7,7 +7,7 @@ title "Hawaii_5-0"
 @set "apk=192681346781"
 @set "app=194321789678"
 @set "iso=192341678321"
-cecho {09}1=apk{/h}2=app{/h}3=iso{04}
+cecho {09}1=apk{\n}2=app{\n}3=iso{04}
 
 set /a m=Choose;
 if (%m%=1) & goto one
